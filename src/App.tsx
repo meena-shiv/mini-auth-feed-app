a20e14ffc4cad6b3b9f58da97962f6a1ec38ea71
Shiv Meena <mshiv0390@gmail.com>
2025-06-20 21:27:29 +0530

    updated base url

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import Footer from './components/Footer';
 
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <Navbar />
       <Routes>
         <Route path="/" element={<Feed />} />
